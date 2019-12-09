762cf68557b25c1a440d6e3259782ce00c9c8688
duanhaodong <duanhaodong@sensetime.com>
2019-12-09 11:03:08 +0800

    fix resizing behavior in spawned sessions

--- auto-ssh/login.sh
+++ auto-ssh/login.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/expect
+trap {
+    set rows [stty rows]
+    set cols [stty columns]
+    stty rows $rows columns $cols < $spawn_out(slave,name)
+} WINCH
 eval spawn ssh username@server
 expect "*Verification code:*"
 send [exec cat dyn_code.txt]
